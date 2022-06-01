4346f334f1d1b79eeeba05b33a33ae64a46c1014
nuricompro <nurettin.altintas@compro.com.tr>
2022-06-01 13:29:40 +0300

    Classes are created and initilized variables and actions

---
+++ Arçelik-ios-week2-homework/Arçelik-ios-week2-homework/InternViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InternViewController.swift
+//  Arçelik-ios-week2-homework
+//
+//  Created by Compro on 1.06.2022.
+//
+
+import UIKit
+
+class InternViewController: UIViewController {
+
+    
+    @IBOutlet weak var resultBMILabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+
+   
+
+}
