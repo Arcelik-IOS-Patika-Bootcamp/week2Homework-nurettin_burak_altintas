1ade5b9bcb857bd4d044111d3fe78005b283c2ce
nuricompro <nurettin.altintas@compro.com.tr>
2022-06-01 12:16:16 +0300

    Initialization of homework

---
+++ Arçelik-ios-week2-homework/Arçelik-ios-week2-homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Arçelik-ios-week2-homework
+//
+//  Created by Compro on 1.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
